fdc42a2f386428bd94db183eb446ce4f6331b6c3
Eldad Bercovici <dutzi.b@gmail.com>
2020-04-17 04:50:17 +0300

    Update facebook-j-k-scrolling.user.js

--- facebook-j-k-scrolling.user.js
+++ facebook-j-k-scrolling.user.js
@@ -3,33 +3,32 @@
 // @namespace   dutzi.party
 // @match       https://www.facebook.com/
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      dutzi
 // @description 4/17/2020, 2:32:33 AM
 // ==/UserScript==
 
 (() => {
   let focusedPost = -1;
-  
+
   function getPostFromFeedUnit(feedUnit) {
     return feedUnit.querySelector('[role="article"]').parentElement;
   }
-  
+
   function getPostAtIndex(index) {
     const feedUnits = document.querySelectorAll('[data-pagelet^="FeedUnit"]');
-    
+
     if (!feedUnits) {
       return;
     }
-    
+
     const feedUnit = feedUnits[focusedPost];
-    
+
     if (!feedUnit) {
       return;
     }
 
     return getPostFromFeedUnit(feedUnit);
-
   }
 
   function highlightFocusedPost({ isScrollingDown }) {
@@ -42,7 +41,10 @@
     post.style.boxShadow = '0px 0px 0px 3px #03A9F4';
     post.style.borderRadius = '10px';
 
-    post.scrollIntoView({ behavior: 'smooth', block: isScrollingDown ? 'end' : 'start' })
+    post.scrollIntoView({
+      behavior: 'smooth',
+      block: isScrollingDown ? 'end' : 'start',
+    });
   }
 
   function concealFocusedPost() {
@@ -68,18 +70,18 @@
       focusedPost = Math.max(focusedPost - 1, 0);
       highlightFocusedPost({ isScrollingDown: false });
     }
-  })
-  
+  });
+
   document.addEventListener('click', (e) => {
     const feedUnit = e.target.closest('[data-pagelet^="FeedUnit"]');
-    
+
     if (!feedUnit) {
       return;
     }
-    
+
     concealFocusedPost();
-    
-    focusedPost = Array.from(feedUnit.parentElement.children).indexOf(feedUnit) - 1;
-    
-  })
-})()
+
+    focusedPost =
+      Array.from(feedUnit.parentElement.children).indexOf(feedUnit) - 1;
+  });
+})();
